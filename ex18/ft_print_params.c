af1a79fc5b208cfa26989ceda30808a15c5e2641
Johnatan <contato.johnatan373@gmail.com>
2022-03-25 12:39:04 -0300

    more

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 17:50:21 by johmatos          #+#    #+#             */
+/*   Updated: 2022/03/25 11:08:50 by johmatos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	main(int argc, char *argv[])
+{
+	int	count;
+	int	str_po;
+
+	str_po = 0;
+	count = 0;
+	while (count <= argc - 1)
+	{
+		while (argv[count][str_po] != '\0' )
+		{
+			ft_putchar(argv[count][str_po]);
+			str_po++;
+		}
+		str_po = 0;
+		ft_putchar('\n');
+		count++;
+	}
+	return (0);
+}
