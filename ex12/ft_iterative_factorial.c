cedac17f8509cf33f6c7faa83191919130124c71
astahjmo <contato.johnatan06@gmail.com>
2022-03-29 00:22:14 -0300

    aaa

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_interative_fatorial.c                           :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 17:21:16 by johmatos          #+#    #+#             */
-/*   Updated: 2022/03/28 23:25:47 by johmatos         ###   ########.fr       */
+/*   Updated: 2022/03/29 00:17:50 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_iterative_factorial(int nb)
 {
 	int	mult;
 
-	if (nb > 0)
+	if (nb < 0)
 		return (0);
 	if (nb == 0)
 		return (1);
